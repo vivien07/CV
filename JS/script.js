105fc58cdd48cfec51bb04c8bb237b8d472db6b1
vivien07 <38786759+vivien07@users.noreply.github.com>
2019-03-20 22:29:08 +0100

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+//SKILLS
+
+$("#title1").mouseover(function() {
+	$("#list1").css("visibility", "visible");
+});
+$("#title2").mouseover(function() {
+	$("#list2").css("visibility", "visible");
+});
+$("#title3").mouseover(function() {
+	$("#list3").css("visibility", "visible");
+});
+$("#title4").mouseover(function() {
+	$("#list4").css("visibility", "visible");
+});
+
+$("#title1").mouseout(function() {
+	$("#list1").css("visibility", "hidden");
+});
+$("#title2").mouseout(function() {
+	$("#list2").css("visibility", "hidden");
+});
+$("#title3").mouseout(function() {
+	$("#list3").css("visibility", "hidden");
+});
+$("#title4").mouseout(function() {
+	$("#list4").css("visibility", "hidden");
+});
